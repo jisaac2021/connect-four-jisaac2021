48ea60f6b020402617db5565bcf4536ec8cab991
jisaac2021 <jisaac2021@kehillahstudent.org>
2019-03-28 15:35:50 -0700

    connectFour classes

--- classes.py
+++ classes.py
@@ -30,11 +30,39 @@ class ConnectFour:
 
 
     def __init__(self, player_1, player_2, rows, cols):
-        pass
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.num_rows = rows
+        self.num_cols = cols
 
+        self.num_turns = 0
+
+        self.board = {}
+        for i in range(self.num_cols):
+            label = string.ascii_uppercase[i]
+            self.board[label] = []
+            print(self.board)
 
     def play_turn(self):
-        pass
+
+        if self.num_turns % 2 == 0:
+            player = self.player_1
+        else:
+            player = self.player_2
+
+        print('%s, it is your turn' % player.name)
+        col = input('Choose a column: ')
+
+        if len(self.board[col]) == self.num_rows:
+            print('Column %s is full!' % col)
+            return False
+        else:
+            self.board[col].append(player.play_chip())
+            self.num_turns += 1
+            return True
 
     def show_state(self):
+        print(self.board)
+
+    def check_win(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 """
 Main game loop
 """
+
+from classes import Player, ConnectFour
+
+p1 = Player('red', 'shlomo')
+p2 = Player('black', 'shlomit')
+
+game = ConnectFour(p1, p2, 6, 7)
+
+while True:
+
+    game.show_state()
+    game.play_turn()
+    game.check_win()
